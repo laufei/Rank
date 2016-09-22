53777109c6a5dce943e3529567b23fc9efa43a07
liufei <qy123456>
2016-09-22 19:10:16 +0800

    init

--- data.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-__author__ = 'liufei'
-
-class data:
-    def __init__(self):
-        self.baidu = "https://www.baidu.com/"
-        self.SearchKeywords = u'曼谷旅游'
-        self.URLKeywords = [u'穷游网']
