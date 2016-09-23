c6691f4ea15010ea53f8dbaa3e2ab96cc15bf296
Lau Fei <goodlf@qq.com>
2016-09-23 14:17:39 +0800

    test

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
-# coding: utf-8
+﻿# coding: utf-8
 __author__ = 'liufei'
 
 class data:
     def __init__(self):
         self.baidu = "https://www.baidu.com/"
         self.SearchKeywords = {
-                                                # u'曼谷旅游':20,
+                                                u'曼谷旅游':20,
                                                 u'新加坡旅游':117,
                                                 u'清迈旅游':17,
                                                 u'大阪旅游':17,
