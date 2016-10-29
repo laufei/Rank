242b277b81f7be744513353b432ef5546ec92169
liufei <qy123456>
2016-10-29 23:17:56 +0800

    使用wx.CallAfter处理GUI展示

--- data.py
+++ data.py
@@ -11,14 +11,3 @@ class data:
         self.proxy_api = "http://www.shandiandaili.com/bindip.aspx?Key=f01064e025ce0a6717db03a8bc4f2712&IP=60.206.194.34"
         self.proxy_txt = "proxy.txt"
         self.URLKeywords = [u'穷游网']
-        self.SearchKeywords = {
-                                                u'曼谷旅游':20,
-                                                u'新加坡旅游':117,
-                                                u'清迈旅游':17,
-                                                u'大阪旅游':17,
-                                                u'首尔旅游': 38,
-                                                u'马来西亚旅游':105,
-                                                u'纽约旅游':56,
-                                                u'荷兰旅游':19,
-                                                u'意大利旅游':29
-        }
