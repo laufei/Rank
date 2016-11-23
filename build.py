9b6bbce35462b1ddd4cfb817e4cf53d57013b892
liufei <qy123456>
2016-11-23 18:34:28 +0800

    build方式加入windows

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 __author__ = 'liufei'
 import os
 # os.system("pyinstaller -F -w --icon='src/icon.ico' rank.py")      # for mac
-# os.system("pyinstaller --onedir -y rank_mac.spec")
-os.system("pyinstaller --onedir -y rank_win.spec")
+os.system("pyinstaller --onedir -y rank_mac.spec")
+# os.system("pyinstaller --onedir -y rank_win.spec")
