b851b193ae68ab808c09aa4b8f651da9e1bb2eb6
liufei <qy123456>
2016-12-02 10:32:13 +0800

    修改build文件

--- builder/build.py
+++ builder/build.py
@@ -2,7 +2,7 @@ __author__ = 'liufei'
 import os
 # os.system("pyinstaller -F -w --icon='../src/icon.ico' ../rank/rank.py")      # for mac
 # os.system("pyinstaller -F -w --onefile --icon='../src/icon.ico' ../rank_req/wxrank_requests.py")        # for windows
-# os.system("pyinstaller --onedir -y rank/rank_mac.spec")
-# os.system("pyinstaller --onedir -y rank/rank_win.spec")
-# os.system("pyinstaller --onedir -y rank_req/rank_requests_mac.spec")
-os.system("pyinstaller --onedir -y ../rank_req/rank_requests_win.spec")
+# os.system("pyinstaller --onefile -y rank/rank_mac.spec")
+# os.system("pyinstaller --onefile -y rank/rank_win.spec")
+os.system("pyinstaller --onefile -y rank_req/rank_requests_mac.spec")
+# os.system("pyinstaller --onefile -y ../rank_req/rank_requests_win.spec")
