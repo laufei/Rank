f3e454a7715c75c3d917d81e38f6703cb9cdfbbc
liufei <qy123456>
2017-02-10 11:23:44 +0800

    增加打开页面时间

--- lib/base.py
+++ lib/base.py
@@ -134,6 +134,7 @@ class base():
             if self.driver.current_url.startswith("http://"):
                 print u"     自动跳转到http页面, 打开该页面失败！"
                 assert False, "Went to http page, failed to open https page! "
+            time.sleep(5)
         except TimeoutException:
             print u"     打开该页面超时！"
             self.driver.execute_script("window.stop()")
